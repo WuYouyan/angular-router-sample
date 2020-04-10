e73bf2c613ffb3be4aeadde8ec71f76054e91fab
Youyan WU <wuyouyan1@gmail.com>
2020-04-10 14:56:11 +0200

    Adding routable animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -18,6 +19,7 @@ import { HeroesModule } from './heroes/heroes.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HeroesModule,
     AppRoutingModule,
