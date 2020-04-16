d37e5289295fcf9bc21cbb8a91e6ea703fa1005d
Youyan WU <wuyouyan1@gmail.com>
2020-04-16 17:01:23 +0200

    Inspect the router's configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { slideInAnimation } from 'src/assets/animations';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +11,12 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'angular-router-sample';
 
+  // Diagnostic only: inspec touter configuration
+  constructor(router: Router) { 
+    // Use a custom replacer to display function names in the route configs
+    const replacer = (key, value) => (typeof value ==='function') ? value.name : value;
+    console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
   getAnimationData(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
   }
