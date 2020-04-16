e1865135010d345f3b0e4fc0b4c148d7243d3947
Youyan WU <wuyouyan1@gmail.com>
2020-04-16 13:47:43 +0200

    Preloading all lazy loaded routes(with loadChidren property)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
@@ -25,7 +25,10 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(
       routes,
-      { enableTracing: true }
+      { 
+        enableTracing: true, // <--debugging purposes
+        preloadingStrategy: PreloadAllModules
+      }
     )
   ],
   exports: [
